7fb1c4c55944386198bcca11302893ecdee23866
Gary Barretto <barretto.gary@gmail.com>
2019-05-24 14:46:48 -0400

    Updates

---
+++ precache-manifest.6f6f7c8cbbdd412d47149750df8c587b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9ddcfed5af3b6da137d",
+    "url": "/ReactApp/static/css/main.c03117a0.chunk.css"
+  },
+  {
+    "revision": "a9ddcfed5af3b6da137d",
+    "url": "/ReactApp/static/js/main.a9ddcfed.chunk.js"
+  },
+  {
+    "revision": "5efb2ccd1f2b7f418809",
+    "url": "/ReactApp/static/js/runtime~main.5efb2ccd.js"
+  },
+  {
+    "revision": "a6e9eae2bcd9069264b2",
+    "url": "/ReactApp/static/css/2.98fc0444.chunk.css"
+  },
+  {
+    "revision": "a6e9eae2bcd9069264b2",
+    "url": "/ReactApp/static/js/2.a6e9eae2.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ReactApp/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ReactApp/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ReactApp/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ReactApp/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ReactApp/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "12128ff17dc29d4f067c67bd9a01fbcd",
+    "url": "/ReactApp/index.html"
+  }
+];
